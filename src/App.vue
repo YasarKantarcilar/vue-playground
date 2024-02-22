a7b89d0a612b8ec8e16046bc76707c8485d8bbac
YasarKantarcilar <kantarcilaryasar@gmail.com>
2024-02-22 12:22:33 +0300

    playing w it

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const list = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+</script>
+
+<template>
+  <div class="w-full gap-4 min-h-screen bg-black grid pt-20 px-[10%] grid-cols-4">
+    <div v-for="number in list" class="aspect-square bg-red-500 bg-white">
+      {{ number }}
+    </div>
+  </div>
+</template>
